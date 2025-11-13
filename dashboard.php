8847b33137a228f7bec2fbaffc28ec0d81dea419
riskacamelia <riskakameliasari17@gmail.com>
2025-11-13 12:47:59 +0700

    logout

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+if( !isset($_SESSION['username']) ) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Dashboard</title>
+</head>
+<body>
+    <h2>SELAMAT DATANG <?= $_SESSION['username'];?></h2>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
